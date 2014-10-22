14cd3dd19260ec6d97913119a4e9ac9df8f50715
Vultor <shawn@shawnroe.com>
2014-10-22 22:29:30 +0900

    Clean JS slate with 4 objects
    bio, work, education, and projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,103 @@
-var name = "Shawn Roe";
-var role = "Web Developer";
+// var name = "Shawn Roe";
+// var role = "Web Developer";
+// var email = "shawn@shawnroe.com";
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedName).append(formattedRole);
-
-var skills = ["aweseomness", "HTML", "teaching", "JS"];
-
-// $("#main").append(skills);
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 var bio = {
-	"name" : name,
-	"role"  : role,
+	"name" : "Shawn Roe",
+	"role"  : "Web Developer",
+	"welcomeMessage" : "Simplifying solar and making things better through the web.",
 	"contact" : {
 		"email" : "shawn@shawnroe.com",
 		"github" : "vultor",
 		"twitter" : "@kunostories",
 		"Location" : "South Korea"
-	}
-	"logoURL" : "/vultor.png",
-	"welcome" : "Hello World!",
-	"skills" : skills
-};
+	},
+	"skills" : ["HTML5", "CSS3", "JS", "PHP", "Business Vision Consultant", "Educator", "Story-teller", "Pitching", "Hitting HR's"]
+}
 
-$("#main").append(bio);
+var education = {
+	"schools": [
+	  {
+		"name": "HARVARD UNIVERSITY Extension School",
+		"location": "Cambridge, MA, USA",
+		"degree": "Masters",
+		"major": ["Liberal Arts in Information Technology"],
+		"dates": "2013-2018",
+		"url": "http://www.extension.harvard.edu/"
+	  },
+	  {
+		"name": "UNIVERSITY OF VIRGINIA",
+		"location": "Charlottesville, VA, USA",
+		"degree": "BA",
+		"major": ["Psychology"],
+		"dates": "2001-2005",
+		"url": "http://www.virginia.edu/"
+	  }
+	],
+	"onlineCourses": [
+	  {
+		"title": "jQuery",
+		"school": "Code Academy",
+		"dates": 2013,
+		"url": "http://www.codecademy.com/en/tracks/jquery"
+	  },
+	  {
+		"title": "Intro to HTML and CSS",
+		"school": "Udacity",
+		"dates": 2014,
+		"url": "https://www.udacity.com/course/ud304"
+	  },
+	  {
+		"title": "Customizing Bootstrap 3 with LESS",
+		"school": "Lynda.com",
+		"dates": 2014,
+		"url": "http://www.lynda.com/sdk/Bootstrap-tutorials/Customizing-Bootstrap-3-LESS/161086-2.html"
+	  }
+	]
+}
+
+var work = {
+	"jobs": [
+	  {
+		"employer": "Chosun University",
+		"title": "Assistant Professor",
+		"location": "Gwangju, South Korea",
+		"dates": 2014,
+		"description": "Teaching English to University undergraduates and adult students at the Language Education Institute, and developing curriculum and classroom materials for a reading and discussion course."
+	  },
+	  {
+		"employer": "Chonnam National University",
+		"title": "English Instructor",
+		"location": "Gwangju, South Korea",
+		"dates": "2012-2013",
+		"description": "Taught English to University professors, undergraduates and adult students at the Language Education Center at Chonnam National University."
+	  },
+	  {
+		"employer": "Dongshin University",
+		"title": "TESOL Instructor, Graduate School Professor, English Conversation Instructor",
+		"location": "Naju, South Korea",
+		"dates": "2010-2012",
+		"description": "Trained TESOL certificate candidates on ‘how to teach writing’, assisted in the development of curriculum and material design for the course, taught IELTS speaking test preparation Graduate School students, conversational English to University undergraduates, and the highest level of speaking to adult Language Education Center students."
+	  }
+	]
+}
+
+var projects = {
+	"projects": [
+	{
+		"title": "SRoeCo Solar Website",
+		"dates": "2008-2014",
+		"description": "SRoeCo Solar is an independent, solar information website that provides objective, verifiable, solar information and seeks to present it in an easily-understandable way. It is committed to simplifying the solar installation process by providing solar knowledge, calculators, and a sortable, searchable solar panel comparison table. SRoeCo Solar simplifies solar to help the average person feel more comfortable making an informed decision to install solar panels. More solar is better.",
+		"images": ["http://www.sroeco.com/solar/sroeco_solar_title.png"]
+	},
+	{
+		"title": "Sinful Treats Bakery",
+		"dates": 2014,
+		"description": "Sinful Treats Bakery is a sweet boutique bakery located in Hampton Roads, Virginia. Chef Stephanie Rolla (Chef Steph) bakes sinfully delicious desserts from scratch just like your Grandma used to make… except she’s not your grandma. Sinful Treats specializes in baked goods like cupcakes, Monster Cookies, birthday and wedding cakes with unique flavor combinations like her famous (with adults) Grand Marnier cake or Guiness cake. Kids love things like our Ninja Turtle birthday cake, and elephant-shaped sugar cookies.",
+		"images": ["http://eatsinfultreats.com/test/wp-content/uploads/2014/02/sinful-treats-logo320x191.png"]
+	}
+	]
+}
